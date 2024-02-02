9222d7ecf015523a72626b09458102017f93a90d
Alex Tymoshenko <oleksandr.tymoshenko@gostudent.org>
2024-02-02 11:34:36 +0100

    Replacing the stateful reference by stateless

--- lib/utils/connectivity/connectivity_stream_builder.dart
+++ lib/utils/connectivity/connectivity_stream_builder.dart
@@ -2,7 +2,7 @@ part of '../../basic_flutter_helper.dart';
 
 typedef ConnectivityStatusBuilder = Widget Function(BuildContext context, bool isOnline);
 
-class ConnectivitySteamBuilder<ConnectivityStatus> extends StatefulWidget {
+class ConnectivitySteamBuilder extends StatelessWidget {
   const ConnectivitySteamBuilder({
     required this.builder,
     super.key,
@@ -11,16 +11,11 @@ class ConnectivitySteamBuilder<ConnectivityStatus> extends StatefulWidget {
   final ConnectivityStatusBuilder builder;
 
   @override
-  State<ConnectivitySteamBuilder> createState() => _ConnectivitySteamBuilderState();
-}
-
-class _ConnectivitySteamBuilderState extends State<ConnectivitySteamBuilder> {
-  @override
   Widget build(BuildContext context) {
     return StreamBuilder<ConnectivityResult>(
       initialData: ConnectivityResult.wifi,
       stream: Connectivity().onConnectivityChanged,
-      builder: (context, snapshot) => widget.builder.call(
+      builder: (context, snapshot) => builder.call(
         context,
         ConnectivityStatus.fromConnectivityResult(snapshot.data!) == ConnectivityStatus.online,
       ),
